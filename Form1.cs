93625f1ebe78739b5a3395938b7ea373b096a1f4
switch90 <serpviso@gmail.com>
2017-01-13 01:10:01 +0300

    Vk авторизация.

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Model.RequestParams;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        private static long UserId;
+        private static string Token = null;
+        private static string NameID = null;
+        private static string SurnameID = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void VkAuth(string login, string pass)
+        {
+            string url = "https://oauth.vk.com/authorize?client_id=5818397&redirect_uri=https://oauth.vk.com/blank.html&scope=messages&display=popup&response_type=token&v=5.62&revoke=1";
+            Form2 form2 = new Form2();
+            form2.Show();
+            WebBrowser browser = (WebBrowser)form2.Controls["webBrowser1"];
+            browser.Navigate(url);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VkAuth(null, null);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader UserIdGet = new StreamReader(File.Open("User_ID.txt", FileMode.Open));
+            UserId = Convert.ToInt64(UserIdGet.ReadLine());
+            UserIdGet.Close();
+            StreamReader TokenGet = new StreamReader(File.Open("Token.txt", FileMode.Open));
+            Token = TokenGet.ReadLine();
+            TokenGet.Close();
+            var vk = new VkApi();
+            vk.Authorize(Token, UserId, 0);
+            var NameGet = vk.Users.Get(UserId);
+            NameID = NameGet.FirstName;
+            SurnameID = NameGet.LastName;
+            label1.Text = "Успешно авторизованно со страницы:" + NameID + " " + SurnameID + ".";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader UserIdGet = new StreamReader(File.Open("User_ID.txt", FileMode.Open));
+            UserId = Convert.ToInt64(UserIdGet.ReadLine());
+            UserIdGet.Close();
+            StreamReader TokenGet = new StreamReader(File.Open("Token.txt", FileMode.Open));
+            Token = TokenGet.ReadLine();
+            TokenGet.Close();
+            var vk = new VkApi();
+            vk.Authorize(Token, UserId, 0);
+            var col = vk.Messages.Send(new MessagesSendParams
+            {
+                UserId = UserId,
+                Message = "Привет, aloha, belissimo",
+            });
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public static string token;
+        public static string id;
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                string url = webBrowser1.Url.ToString();
+                string l = url.Split('#')[1];
+                if (l[0] == 'a')
+                {
+                    token = l.Split('&')[0].Split('=')[1];
+                    id = l.Split('=')[3];
+                    StreamWriter SW = new StreamWriter(new FileStream("User_ID.txt", FileMode.Create, FileAccess.Write));
+                    SW.Write(id);
+                    SW.Close();
+                    StreamWriter SW2 = new StreamWriter(new FileStream("Token.txt", FileMode.Create, FileAccess.Write));
+                    SW2.Write(token);
+                    SW2.Close();
+                    if (token != "")
+                    {
+                        this.Hide();
+                        MessageBox.Show("Авторизация удалась");
+                    }
+                }
+            }
+
+            catch
+            {
+
+
+            }
+        }
+    }
+}
+
